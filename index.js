c97e8eff20b0c78e86e0c1bec53343ea5cc563d8
irhamhds <irhds101@gmail.com>
2023-05-14 21:57:56 +0700

    Coffee serang

---
+++ index.js
@@ -0,0 +1,15 @@
+// const coffeeStock = require('./state');
+
+// console.log(coffeeStock);
+
+// console.log("haloo")l
+// console.log("hay")
+// console.log("my name is")
+// console.log("hadis")
+
+const _ = require ('lodash');
+ 
+const myArray = [1, 2, 3, 5];
+myArray.shift(9)
+
+console.log(myArray);
